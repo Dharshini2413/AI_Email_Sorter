708359584d34d0f5b5fbd53794d775452bab0f6c
Dharshini2413 <66021178+Dharshini2413@users.noreply.github.com>
2026-02-28 14:36:55 +0530

    Update List_Gemini_models.py

--- List_Gemini_models.py
+++ List_Gemini_models.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyDlzD8SWJpGi-Jw6uxP6DLsb4vtkU7Jb90")
+genai.configure(api_key="<your_api_key>")
 
 for m in genai.list_models():
     if "generateContent" in m.supported_generation_methods:
